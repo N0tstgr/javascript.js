5f46b081fca14ed11a7125d04d6e282a99687bb9
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-08 21:57:08 +0530

    ch4

---
+++ Not_ch4_String.js
@@ -0,0 +1,15 @@
+let name = "vikash"
+// console.log(name.length)
+console.log(name[0])
+console.log(name[1])
+console.log(name[2])
+let friend = 'vikash'
+console.log(friend)
+// let myfriend = "juxxy"  --> do not do this
+// console.log(myfriend)
+
+//Template literals
+let boy1 = "zuxxy"
+let boy2 = "microboy"
+let sentence = `${boy2} is the teammate of ${boy1}`
+console.log(sentence)
