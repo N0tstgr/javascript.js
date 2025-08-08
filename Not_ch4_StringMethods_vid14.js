c8b62ae85cf8c486c6e7ded4e2439922cb6c1174
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-08 23:01:45 +0530

    stringM

--- Not_ch4_StringMethods_vid14.js
+++ Not_ch4_StringMethods_vid14.js
@@ -1,10 +1,22 @@
 // let name  = "vikashthetrillionaire"
-let name = "luxxy"
-console.log(name.length)
-// process.stdout.write("Hello \r World")
-console.log(name.toUpperCase())
-console.log(name.toLowerCase())
-console.log(name.slice(2,4))
-console.log(name.slice(2))
-let newname = name.replace("xy", "or")
-console.log(newname)
+let name = "   luxxy   "
+// console.log(name.length)
+// // process.stdout.write("Hello \r World")
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.slice(2,4))
+// console.log(name.slice(2))
+// let newname = name.replace("xy", "or")
+// console.log(newname)
+// let sur1 = "kasaudhan"
+// let sur2 = "vikash"
+// let sur3 = sur1.concat(sur2, "thetrillionaire")
+// console.log(sur3)
+// let newName = name.trim()
+// console.log(newName)
+
+let member = "btr"
+console.log(name.concat("  is a member of ", member, " Ok"))
+let colleague = "      not             "
+console.log(colleague)
+console.log(colleague.trim())
