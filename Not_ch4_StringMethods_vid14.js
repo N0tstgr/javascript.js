42b9149066275e5839c6bd87a9e06485233cb0dc
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-08 22:42:17 +0530

    string

---
+++ Not_ch4_StringMethods_vid14.js
@@ -0,0 +1,10 @@
+// let name  = "vikashthetrillionaire"
+let name = "luxxy"
+console.log(name.length)
+// process.stdout.write("Hello \r World")
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.slice(2,4))
+console.log(name.slice(2))
+let newname = name.replace("xy", "or")
+console.log(newname)
--- Not_ch4_String_vid13.js
+++ Not_ch4_String_vid13.js
@@ -13,3 +13,9 @@ let boy1 = "zuxxy"
 let boy2 = "microboy"
 let sentence = `${boy2} is the teammate of ${boy1}`
 console.log(sentence)
+
+//Escape sequence Characters--> backslash is a also a character but backslash
+let fruit  = 'Bana\'na'
+let kali = "Adam D\"Angelo"
+console.log(fruit.length)
+console.log(kali)
