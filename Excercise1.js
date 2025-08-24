de682fb442527db310e3733204709e3d82f01f4c
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-24 08:34:46 +0530

    Excercise1

---
+++ Excercise1.js
@@ -0,0 +1,25 @@
+const prompt = require("prompt-sync")();
+
+let a = Math.floor(Math.random()*10)+1
+console.log(a)
+let guesses = 0
+
+while(true){
+
+let b = Number(prompt("Enter the number to be guessed  in range of (1-100! : "))
+guesses++
+
+
+if(a == b){
+    let score = 100-guesses
+console.log("This is a correct number",a);
+console.log(" Booyah ! your score is " ,score,"in", guesses, "attemepts")
+break
+}
+else if(b>a){
+    console.log("This is a greater number than the original number", a)
+}else if(b<a){
+    console.log("This number is lesser than the original number",a)
+}
+
+}
