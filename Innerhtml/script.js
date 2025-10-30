f42e70abf52bb31b9453a98f48f24bd9a234690b
Your Name <vikashkasaudhan321@gmail.com>
2025-10-30 11:29:26 +0530

    innerHtml

---
+++ Innerhtml/script.js
@@ -0,0 +1,16 @@
+let x = document.getElementsByTagName('span')[0];
+console.log(x)
+let c = document.getElementsByTagName('span')[0];
+console.dir(c)
+
+console.log(document.body.firstElementChild.nodeName)
+console.log(document.body.firstChild.nodeName)
+second.innerHTML = "<i> hey this is italic </i>"
+second.innerHTML = "<i> hey this is italic </i>"
+'<i> hey this is italic </i>'
+second.outerHTML
+'<span id="second"><i> hey this is italic </i></span>'
+second.outerHTMl = "<div> ey </div>
+
+second.outerHTMl = "<div> ey </div>"
+'<div> ey </div>'
