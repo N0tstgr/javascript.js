1541094d1fb33c82e087c2c7917d02629952eaa9
Your Name <vikashkasaudhan321@gmail.com>
2025-10-30 11:43:22 +0530

    hidden

--- Innerhtml/script.js
+++ Innerhtml/script.js
@@ -1,16 +1,19 @@
-let x = document.getElementsByTagName('span')[0];
-console.log(x)
-let c = document.getElementsByTagName('span')[0];
-console.dir(c)
+// let x = document.getElementsByTagName('span')[0];
+// console.log(x)
+// let c = document.getElementsByTagName('span')[0];
+// console.dir(c)
 
-console.log(document.body.firstElementChild.nodeName)
-console.log(document.body.firstChild.nodeName)
-second.innerHTML = "<i> hey this is italic </i>"
-second.innerHTML = "<i> hey this is italic </i>"
-'<i> hey this is italic </i>'
-second.outerHTML
-'<span id="second"><i> hey this is italic </i></span>'
-second.outerHTMl = "<div> ey </div>"
+// console.log(document.body.firstElementChild.nodeName)
+// console.log(document.body.firstChild.nodeName)
+// second.innerHTML = "<i> hey this is italic </i>"
+// second.innerHTML = "<i> hey this is italic </i>"
+// '<i> hey this is italic </i>'
+// second.outerHTML
+// '<span id="second"><i> hey this is italic </i></span>'
+// second.outerHTMl = "<div> ey </div>"
 
-second.outerHTMl = "<div> ey </div>"
-'<div> ey </div>'
+// second.outerHTMl = "<div> ey </div>"
+// '<div> ey </div>'
+
+
+console.log(document.body.textContent)
