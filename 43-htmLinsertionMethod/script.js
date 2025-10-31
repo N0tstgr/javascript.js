0c8bf0a72fe72843505533cb316ceb7e1a8d780f
N0tstgr <vikashkasaudhan321@gmail.com>
2025-10-31 08:19:37 +0530

    morningses

--- 43-htmLinsertionMethod/script.js
+++ 43-htmLinsertionMethod/script.js
@@ -1,7 +1,7 @@
 let a =   document.getElementByTagName('div')[0]
 // a.innerHTML = a.innerHTML + '<h1>Hello World!</h1>';
 
-// let div = document.createElement('div');
-// div.innerhtml = '<h1> hello vikash the trillionaire </h1>';
-// a.appendChild(div)
+let div = document.createElement('div');
+div.innerhtml = '<h1> hello vikash the trillionaire </h1>';
+a.append(div)
 
