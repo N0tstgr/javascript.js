7d6865f7cd6a7327cea2281f9baadcfadb1d2572
N0tstgr <vikashkasaudhan321@gmail.com>
2025-10-31 08:06:31 +0530

    createElement

--- 43-htmLinsertionMethod/script.js
+++ 43-htmLinsertionMethod/script.js
@@ -1,2 +1,5 @@
 let a =   document.getElementByTagName('div')[0]
-a.innerHTML = a.innerHTML + '<h1>Hello World!</h1>';
+// a.innerHTML = a.innerHTML + '<h1>Hello World!</h1>';
+
+let div = document.createElement('div');
+div.innerhtml = '<h1> hello vikash the trillionaire </h1>';
