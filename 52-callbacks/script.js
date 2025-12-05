4ecf4dc4310e0fee2af4ba3de8035d2875320ac9
N0tstgr <vikashkasaudhan321@gmail.com>
2025-12-05 18:10:21 +0530

    callback-syncronous

---
+++ 52-callbacks/script.js
@@ -0,0 +1,6 @@
+//Syncronous programming 
+
+let a = prompt("what is your name")
+let b = prompt("what is your age")
+let c = prompt("what is your favorite color?")
+console.log(a + " is " + b + " years old and has " + c + " favorite color.")
