3835885b53ca34a29a6e4bd04e3cdb4c6f2d4748
N0tstgr <vikashkasaudhan321@gmail.com>
2025-12-05 18:18:21 +0530

    Asynchronous

--- 52-callbacks/script.js
+++ 52-callbacks/script.js
@@ -1,6 +1,16 @@
-//Syncronous programming 
+//Synchronous programming 
 
-let a = prompt("what is your name")
-let b = prompt("what is your age")
-let c = prompt("what is your favorite color?")
-console.log(a + " is " + b + " years old and has " + c + " favorite color.")
+// let a = prompt("what is your name")
+// let b = prompt("what is your age")
+// let c = prompt("what is your favorite color?")
+// console.log(a + " is " + b + " years old and has " + c + " favorite color.")
+
+//Asynchronous programming
+
+console.log("Start")
+setTimeout(function(){
+    console.log("this is to be run after 5 seconds")
+},5000
+
+)
+console.log("this is the ending")
