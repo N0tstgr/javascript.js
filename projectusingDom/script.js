7fdf58e9026d2d0185c571fd2d1014ef6a8c19cd
Vikash Kasaudhan <vikashkasaudhan321@gmail.com>
2026-03-03 20:31:59 +0530

    locale

--- projectusingDom/script.js
+++ projectusingDom/script.js
@@ -1,4 +1,11 @@
 const timer = document.getElementById('root');
 const now = new Date();
-timer.innerHTML = now;
+const IndianTime = now.toLocaleTimeString();
+timer.innerHTML = IndianTime;
+timer.style.fontSize = "100px";
+timer.style.display  = "flex";
+timer.style.justifyContent = "center";
+timer.style.alignItems = "center";
+timer.style.backgroundColor = "orange";
+
 
